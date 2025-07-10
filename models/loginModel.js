4fa0c4d10631d8934b180cbd5cca71fa2d736d2b
Anshul <anshul007saxena@gmail.com>
2025-07-10 14:12:22 +0530

    add authontication and login

---
+++ models/loginModel.js
@@ -0,0 +1,57 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+
+const loginSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  password: {
+    type: String,
+    required: true,
+    select: false,
+    minlength: [8, 'Password must be at least 8 characters long'],
+    validate: {
+      validator: function (value) {
+        return /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?#&^])[A-Za-z\d@$!%*?#&^]{8,}$/.test(value);
+      },
+      message:
+        'Password must include uppercase, lowercase, number, and special character.'
+    }
+  },
+  role: {
+    type: String,
+    enum: ['Admin', 'SubAdmin', 'User'],
+    default: 'User'
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
+  },
+  ipAddressLog: [
+    {
+      ip: { type: String },
+      date: { type: Date, default: Date.now }
+    }
+  ]
+});
+
+loginSchema.pre('save', async function (next) {
+  if (!this.isModified('password')) return next();
+
+  try {
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+    return next();
+  } catch (err) {
+    return next(err);
+  }
+});
+
+
+loginSchema.methods.comparePassword = async function (candidatePassword) {
+  return await bcrypt.compare(candidatePassword, this.password);
+};
+
+module.exports = mongoose.model('Login', loginSchema);
