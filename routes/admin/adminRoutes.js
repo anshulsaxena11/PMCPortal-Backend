53637a05805ae7f87c50d5032672ecc8cc09af03
Anshul <anshul007saxena@gmail.com>
2025-07-15 16:41:54 +0530

    setsubAdminRole

--- routes/admin/adminRoutes.js
+++ routes/admin/adminRoutes.js
@@ -4,14 +4,14 @@ const adminCtrl = require('../../controller/admincontroller')
 const authMiddleware = require('../../middleware/authMiddleware');
 const allowRoles = require("../../middleware/allowRole");
 
-routes.post('/syncEmp',adminCtrl.sync);
+routes.post('/syncEmp',authMiddleware,allowRoles('Admin'),adminCtrl.sync);
 routes.get('/empList', authMiddleware, allowRoles('Admin'), adminCtrl.getStpiEmpList);
-routes.put('/empList',adminCtrl.empMapping)
-routes.get('/stpiCentre',adminCtrl.stpiCentre)
-routes.get('/srpiEmpType',adminCtrl.stpiEmpType)
-routes.get('/stpiDirectorates',adminCtrl.stpidir)
-routes.put('/taskMember',adminCtrl.taskForceMemberStatus)
-routes.post('/register',adminCtrl.register)
+routes.put('/empList',authMiddleware,allowRoles('Admin'),adminCtrl.empMapping)
+routes.get('/stpiCentre',authMiddleware,allowRoles('Admin','SubAdmin'),adminCtrl.stpiCentre)
+routes.get('/srpiEmpType',authMiddleware,allowRoles('Admin','SubAdmin'),adminCtrl.stpiEmpType)
+routes.get('/stpiDirectorates',authMiddleware,allowRoles('Admin','SubAdmin'),adminCtrl.stpidir)
+routes.put('/taskMember',authMiddleware,allowRoles('Admin'),adminCtrl.taskForceMemberStatus)
+routes.post('/register',authMiddleware,allowRoles('Admin'),adminCtrl.register)
 routes.post('/login', adminCtrl.login);
 routes.get('/validate',authMiddleware)
 routes.post('/logout',adminCtrl.logout)
